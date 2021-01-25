d10a6f69a35f5ab4e1654a92db95dbcd51732475
franciscogomestv@gmail.com <franciscogomestv@gmail.com>
2021-01-25 15:51:17 +0000

    wrote the function and respective tests

--- __tests__/chunklify.test.js
+++ __tests__/chunklify.test.js
@@ -1,13 +1,35 @@
 const chunklify = require('../');
 
 describe('chunklify', () => {
-  it('function chunklify exists', () => {});
+  it('function chunklify exists', () => {
+    expect(typeof chunklify).toEqual('function');
+  });
 
-  it('splits an array of 5 elements with chunk size 1', () => {});
+  it('splits an array of 5 elements with chunk size 1', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const chunked = chunklify(arr, 1);
 
-  it('splits an array of 9 elements with chunk size 2 while having 1 element as remainder', () => {});
+    expect(chunked).toEqual([[1], [2], [3], [4], [5]]);
+  });
 
-  it('splits an array of 7 elements with chunk size 3', () => {});
+  it('splits an array of 9 elements with chunk size 2 while having 1 element as remainder', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const chunked = chunklify(arr, 2);
 
-  it('splits an array of 11 elements with chunk size 5', () => {});
+    expect(chunked).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9]]);
+  });
+
+  it('splits an array of 7 elements with chunk size 3', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7];
+    const chunked = chunklify(arr, 3);
+
+    expect(chunked).toEqual([[1, 2, 3], [4, 5, 6], [7]]);
+  });
+
+  it('splits an array of 11 elements with chunk size 5', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const chunked = chunklify(arr, 5);
+
+    expect(chunked).toEqual([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11]]);
+  });
 });
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function chunklify(array, size) {}
+function chunklify(array, size) {
+  let arr = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    let chunk = array.slice(i, i + size);
+    arr.push(chunk);
+  }
+
+  return arr;
+}
 
 module.exports = chunklify;
